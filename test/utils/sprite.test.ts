ae5db7b7f34359c75a7d1a7ad86306d4c9dcfb63
Bob Yeo <ryeo@slync.io>
2022-01-03 17:19:15 -0800

    Add initial sprite tests for intersect

--- test/utils/sprite.test.ts
+++ test/utils/sprite.test.ts
@@ -2,13 +2,29 @@ import { Sprite } from 'pixi.js'
 import { SpriteUtils } from '../../src/utils/sprite'
 
 describe('#intersect', ( ) => {
-  const spriteA = {
-    x: 0,
-    y: 0,
-    height: 20,
-    width: 20
-  }
-  it('detects a collision', () => {
-    expect(SpriteUtils.intersect(spriteA as Sprite, spriteA as Sprite)).toBe.bind(true)
+  const mainSprite =  new Sprite()
+  const spriteSameHeightWideRight = new Sprite()
+  const spriteDirectlyBelow = new Sprite()
+  beforeAll(() => {
+    spriteSameHeightWideRight.height = 20
+    spriteSameHeightWideRight.width = 20
+    spriteSameHeightWideRight.x = 21
+    spriteSameHeightWideRight.y = 0
+    mainSprite.x = 0,
+    mainSprite.y = 0,
+    mainSprite.height = 20,
+    mainSprite.width =20
+    spriteDirectlyBelow.height = 20
+    spriteDirectlyBelow.width = 20
+    spriteDirectlyBelow.x = 0
+    spriteDirectlyBelow.y = 21
+  })
+
+  it('does not detect a collision with a sprite at same y but outside x', () => {
+    expect(SpriteUtils.intersect(mainSprite, spriteSameHeightWideRight)).toBe(false)
+  })
+
+  it('does not detect a collision with a sprite outside y but same x', () => {
+    expect(SpriteUtils.intersect(mainSprite, spriteDirectlyBelow)).toBe(false)
   })
 })
